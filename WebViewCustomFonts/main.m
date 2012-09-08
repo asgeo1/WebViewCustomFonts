4adedd63004803a0a5bd0d47bcd813390349619b
Nandeep mali <nandeep@miniorb.in>
2012-09-08 15:36:51 +0530

    Initial Commit

---
+++ WebViewCustomFonts/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  WebViewCustomFonts
+//
+//  Created by Nandeep Mali on 08/09/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
